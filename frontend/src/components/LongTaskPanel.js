85336c3d73e4a248a251ed9bb30930e4ecedddc5
MuriloUnten <murilo.unten@proton.me>
2026-01-03 13:11:31 -0300

    refactor(frontend): refactor LongTaskPanel component to use generic poller

--- frontend/src/components/LongTaskPanel.js
+++ frontend/src/components/LongTaskPanel.js
@@ -1,43 +1,19 @@
 import { useState } from "react";
 import { pollAsyncTask, startAsyncTask } from "../api";
 import { useToast } from "../ToastProvider";
+import { useTaskPoller } from "../hooks/useTaskPoller";
 
 /**
  * LongTaskPanel allows for starting and tracking an asycnhronous task.
  * Once the task is started, the component polls for the task's status every 2 seconds.
  */
 export default function LongTaskPanel() {
-    const notify = useToast();
-    const [status, setStatus] = useState("NOT STARTED");
     const [taskId, setTaskId] = useState("");
+    const { taskState, taskResult } = useTaskPoller(taskId, 2000);
 
     async function startTask() {
-        const task = await startAsyncTask();
-        if (!task) {
-            notify("Failed to start task.", "danger");
-            return;
-        }
-
-        setTaskId(task.task_id);
-        setStatus(task.status);
-
-        const polling = setInterval(async () => {
-            const response = await pollAsyncTask(task.task_id);
-            setStatus(response.state);
-
-            if (response.state === "RETRY") {
-                notify("Async task possibly failed. Please retry.", "warning", 0);
-                clearInterval(polling);
-            }
-            else if (response.state === "FAILURE") {
-                notify("Async task failed.", "danger", 0);
-                clearInterval(polling);
-            }
-            else if (response.state === "SUCCESS") {
-                notify("Async task finished successfully.", "success");
-                clearInterval(polling);
-            }
-        }, 2000);
+        const res = await startAsyncTask();
+        setTaskId(res.task_id);
     }
 
     return (
@@ -46,11 +22,11 @@ export default function LongTaskPanel() {
             <button
                 onClick={startTask}
                 className="btn btn-warning"
-                disabled={status === "accepted" || status === "PENDING" || status === "STARTED"}
+                disabled={taskState === "accepted" || taskState === "PENDING" || taskState === "STARTED"}
             >
                 Start Task
             </button>
-            <div>Status: {status}</div>
+            <div>Status: {taskState}</div>
             <div className="mb-2">
                 <strong>Task ID:</strong>
                 <br />
